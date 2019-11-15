42422604859e2b3db7bed816dc4225f027a28036
Daryl <dar.salinas@alumnos.duoc.cl>
2019-11-14 23:18:49 -0300

    solucion error del templade

--- MascoPet/settings.py
+++ MascoPet/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'MascoPet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'apps/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
