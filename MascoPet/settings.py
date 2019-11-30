d17aa1aeea2ccde4aa1cb138a9d6932c44fd9f81
Daryl <dar.salinas@alumnos.duoc.cl>
2019-11-30 20:52:46 -0300

    agregar la API

--- MascoPet/settings.py
+++ MascoPet/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'apps.Producto',
     'apps.Usuario',
     'apps.Registro',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 LOGIN_REDIRECT_URL = reverse_lazy('index')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
--- apps/Producto/urls.py
+++ apps/Producto/urls.py
@@ -1,13 +1,25 @@
 from django.urls import path, include
 from apps.Producto.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, alimentosGatos
 from django.contrib.auth.decorators import login_required
-
+from django.conf.urls import url, include
+from rest_framework import routers
+from apps.Producto.quickstart import views
+ 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+ 
 
 urlpatterns = [
-     path('', index,name='index'),
+     path('', login_required (index),name='index'),
      path('alimentosGatos', login_required (alimentosGatos), name='alimentosGatos'),
      path('nuevo', login_required (mascota_view), name='mascota_crear'),
      path('listar', login_required (mascota_list), name='mascota_listar'),
      path('editar/<int:id_producto>',login_required (mascota_edit), name='mascota_editar'),
      path('eliminar/<int:id_producto>',login_required (mascota_delete), name='mascota_eliminar'),
+     path('api',include(router.urls)),
+     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+
+
